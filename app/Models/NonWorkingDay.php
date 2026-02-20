8126dfbbff1e4a2ac7c9daf55c6819a1eee4aade
Ali Shaalan <shaalanali20@gmail.com>
2026-02-20 12:51:03 +0200

    NonWorkingDays Model,Name field

---
+++ app/Models/NonWorkingDay.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NonWorkingDay extends Model
+{
+    use HasFactory;
+        protected $fillable = [
+        'date',
+        'name',
+        'reason'
+    ];
+
+    protected $casts = [
+        'date' => 'date',
+    ];
+}
