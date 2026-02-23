ab2993082ed68df946ecdda9fc057d59e9d639cd
Ali Shaalan <shaalanali20@gmail.com>
2026-02-23 14:09:18 +0200

    Password reset,forgot

---
+++ app/Http/Controllers/ResetPasswordController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Hash;
+
+class ResetPasswordController extends Controller
+{
+    public function reset(Request $request)
+    {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password),
+                    'must_change_password' => false,
+                ])->save();
+            }
+        );
+
+        return $status === Password::PASSWORD_RESET
+            ? response()->json(['message' => 'Password reset successful'])
+            : response()->json(['message' => 'Invalid token'], 400);
+    }
+}
